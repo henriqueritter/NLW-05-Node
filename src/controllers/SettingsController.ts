2b592f973bddd5f0f76fa43b7bb640f582d1c7ad
Henrique Ritter <henrique-ritter@hotmail.com>
2021-04-21 10:31:14 -0300

    NLW 05 Day 2, Control, Entity, DB

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+class SettingsController {
+  async create(request: Request, response: Response) {
+    const { chat, username } = request.body;
+
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+      chat,
+      username,
+    });
+
+    await settingsRepository.save(settings);
+
+    return response.json(settings);
+  }
+
+  async show(request: Request, response: Response) {
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = await settingsRepository.find();
+
+    if (!settings) {
+      return response.json({ message: "Cant find any Settings" });
+    }
+
+    return response.json(settings);
+  }
+}
+
+export { SettingsController };
